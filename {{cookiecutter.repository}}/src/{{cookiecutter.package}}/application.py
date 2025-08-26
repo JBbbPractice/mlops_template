065eb789fcc5d043353e6d27b58fc4f6bfc201b2
JBbbPractice <jeroenbleker@hotmail.com>
2025-08-26 13:41:37 +0200

    fix(repo): resolved style issues

--- {{cookiecutter.repository}}/src/{{cookiecutter.package}}/application.py
+++ {{cookiecutter.repository}}/src/{{cookiecutter.package}}/application.py
@@ -1,20 +1,26 @@
 # ACTUAL APPLICATION OR SYSTEM LOGIC
 import typer
 
-def main(input: typer.argument(help="Input"), output_dir: typer.argument(help="Output directory"), verbose: bool=typer.Option(False, "--verbose", "-v", help="Verbosity of the output")):
-         """Workflow documentation"""
-         # Start workflow, include output when verbose
-        if verbose:
-            typer.echo(f"Processing {input}")
 
-        # ADD LOGIC
+def main(
+    input_path: str = typer.Argument(help="Input"),
+    output_dir: str = typer.Argument(help="Output directory"),
+    verbose: bool = typer.Option(
+        False, "--verbose", "-v", help="Verbosity of the output"
+    ),
+):
+    """Workflow documentation"""
+    # Start workflow, include output when verbose
+    if verbose:
+        typer.echo(f"Processing {input_path}")
 
-        # End workflow, again  include output when verbose
-        if verbose:
-            typer.echo("Workflow completed.")
+    # ADD LOGIC
+
+    # End workflow, again include output when verbose
+    if verbose:
+        typer.echo("Workflow completed.")
 
 
 # For optional cmd execution
 if __name__ == "__main__":
     typer.run(main)
-        
